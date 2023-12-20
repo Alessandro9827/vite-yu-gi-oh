0c1eeba8149293ac0acdf708562b05b34d612244
Alessandro9827 <ale.moramarco27@gmail.com>
2023-12-20 20:09:26 +0100

    crated scffolding

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <nav class="navbar bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+            <img src="../assets/img/Yugioh_anime_logo.webp" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+            {{ title }}
+            </a>
+        </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: 'Yu-gi-oh Api'
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,19 @@
+<template lang="">
+  <main>
+    <CardsWrapper />
+  </main>
+</template>
+
+<script>
+import CardsWrapper from './CardsWrapper.vue';
+
+export default {
+    components: {
+        CardsWrapper
+    }
+}
+</script>
+
+<style>
+
+</style>
